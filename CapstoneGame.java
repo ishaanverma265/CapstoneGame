b08ee94ae2eed5c4a4a96f626a15e7aa064245bf
GooeyDoggo <89432707+ishaanverma265@users.noreply.github.com>
2022-04-15 21:03:52 -0700

    2 New Files

---
+++ CapstoneGame.java
@@ -0,0 +1,11 @@
+import java.awt.Canvas;
+public class CapstoneGame extends Canvas {
+	public static void main (String[] args) {
+		new Window(800, 600, "FireFighters Prototype", new CapstoneGame());
+	}
+
+	public static void start() {
+		// TODO Auto-generated method stub
+		
+	}
+}
