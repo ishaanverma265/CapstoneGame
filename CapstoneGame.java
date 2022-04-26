317c6210c496910e3f68166e3d7f4c5db37f9f7e
GooeyDoggo <89432707+ishaanverma265@users.noreply.github.com>
2022-04-26 14:30:00 -0700

    Done stuff

--- CapstoneGame.java
+++ CapstoneGame.java
@@ -1,5 +1,15 @@
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 public class CapstoneGame extends Canvas {
+	
+	public CapstoneGame () {
+		setBackground(Color.black);
+	}
+	
 	public static void main (String[] args) {
 		new Window(800, 600, "FireFighters Prototype", new CapstoneGame());
 	}
@@ -8,4 +18,22 @@ public class CapstoneGame extends Canvas {
 		// TODO Auto-generated method stub
 		
 	}
+	
+    public void paint (Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        
+        // Sets the center of the screen to be at the center of the screen.
+        int width = getWidth();
+        int height = getHeight();
+        int xCenter = width / 2;
+        int yCenter = height / 2;
+        
+        // Changes the font to be bolded.
+        Font comicSansBolded = new Font("Comic Sans", Font.BOLD, 35);
+        
+        // Draws the title screen.
+        g2.setColor(Color.red);
+        g2.setFont(comicSansBolded);
+        g2.drawString ("FireFighers Prototype", xCenter - 125, yCenter - 100);
+     }
 }
--- Window.java
+++ Window.java
@@ -1,22 +1,36 @@
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import javax.swing.JFrame;
 
-public class Window extends Canvas{
+
+public class Window extends Canvas {
 	CapstoneGame game = new CapstoneGame();
 	public Window (int width, int height, String title, CapstoneGame game) {
 		JFrame frame = new JFrame(title);
+		
+		// The frame is set to a constant size of width and height, hence you cannot expand or shrink the window.
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+		
+		// Closes the window.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Window appears in the center of the screen.
 		frame.setLocationRelativeTo(null);
+		
+		// Calls the CapstoneGame class and allows for the window to be seen and be able to run.
 		frame.add(game);
 		frame.setVisible(true);
-		CapstoneGame.start();
+		CapstoneGame.start();	
+	}
+	
+	public void paint (Graphics g) {
 		
 	}
 }
